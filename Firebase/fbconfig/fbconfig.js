1b9de1c1ab177b65f8fc73fcd329c5876540fb53
tomastomvi <vitomas2007@gmail.com>
2025-12-01 20:31:38 -0300

    fist

---
+++ Firebase/fbconfig/fbconfig.js
@@ -0,0 +1,14 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyDmB_DQ-H2rJdwJXBOfta-dvBrJ8oA2uF8",
+    authDomain: "orcfacil.firebaseapp.com",
+    projectId: "orcfacil",
+    storageBucket: "orcfacil.firebasestorage.app",
+    messagingSenderId: "919075396686",
+    appId: "1:919075396686:web:6792fa0dc74fcce605b71f",
+    measurementId: "G-5EEVQTGGQY"
+  };
+
+  window.firebaseConfig = firebaseConfig;
+
---
+++ Firebase/fbconfig/main.js
@@ -0,0 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
+
+
+const app = initializeApp(window.firebaseConfig);
+const analytics = getAnalytics(app);
